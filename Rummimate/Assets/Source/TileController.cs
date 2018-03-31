e8c89f08bf9418a77a73dbcc19c70a18af8f8c65
Bartosz Rudzki (Home) <bartosz.rudzki@baadgames.com>
2018-03-31 23:27:41 +0200

    Name convention changed. Simple tile and background added.

---
+++ Rummimate/Assets/Source/TileController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("MouseDown");
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pz.z = 0;
+        gameObject.transform.position = pz;
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("MouseUp");
+    }
+}
