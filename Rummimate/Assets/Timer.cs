4e5373a237095168b57547c41497c6fba297ea5c
Bartosz Rudzki (Home) <bartosz.rudzki@baadgames.com>
2018-04-03 10:58:04 +0200

    Beeps added. Timer tweaks.

--- Rummimate/Assets/Timer.cs
+++ Rummimate/Assets/Timer.cs
@@ -12,6 +12,11 @@ public class Timer : MonoBehaviour {
     public TMP_InputField Minutes;
     public TMP_InputField Seconds;
 
+    public AudioSource EndTimeSound;
+    public AudioSource Beep5Second;
+    public AudioSource Beep10Second;
+
+    private const string SAVE_TIME_KEY = "SaveTime";
     private float _savedTime = 0;
     private float _curTime = 0;
     public bool IsOn { private set; get; }
@@ -24,6 +29,9 @@ public class Timer : MonoBehaviour {
         Seconds.onEndEdit.AddListener(EndEditHandler);
 
         IsOn = false;
+        _savedTime = PlayerPrefs.GetFloat(SAVE_TIME_KEY);
+        _curTime = _savedTime;
+        SetTime(_curTime);
     }
 
     private void OnDestroy()
@@ -32,17 +40,27 @@ public class Timer : MonoBehaviour {
         RestartButton.onClick.RemoveListener(RestartHandler);
         Minutes.onEndEdit.RemoveListener(EndEditHandler);
         Seconds.onEndEdit.RemoveListener(EndEditHandler);
+
+        PlayerPrefs.SetFloat(SAVE_TIME_KEY, _savedTime);
     }
 
     private void StartStopHandler()
     {
-        IsOn = !IsOn;
+        if(EndTimeSound.isPlaying)
+        {
+            TurnEndTimeSound(false);
+        }
+        else
+        {
+            IsOn = !IsOn;
+        }
     }
 
     private void RestartHandler()
     {
         _curTime = _savedTime;
         IsOn = true;
+        TurnEndTimeSound(false);
     }
 
     private void EndEditHandler(string str)
@@ -64,25 +82,50 @@ public class Timer : MonoBehaviour {
     {
         if(IsOn)
         {
+            float prev = _curTime;
             _curTime -= Time.deltaTime;
-            if(_curTime < 0)
+
+            CheckBeep(prev, _curTime, 5, Beep5Second);
+            CheckBeep(prev, _curTime, 10, Beep10Second);
+
+            if (_curTime < 0)
             {
                 _curTime = 0;
                 IsOn = false;
-                PlayEndTimeSound();
+                TurnEndTimeSound(true);
             }
 
-            int seconds = (int)_curTime;
-            int minutes = seconds / 60;
-            seconds %= 60;
-
-            Minutes.text = minutes.ToString();
-            Seconds.text = seconds.ToString();
+            SetTime(_curTime);
         }
     }
 
-    private void PlayEndTimeSound()
+    private void SetTime(float curTime)
     {
-        
+        int seconds = (int)curTime;
+        int minutes = seconds / 60;
+        seconds %= 60;
+
+        Minutes.text = minutes.ToString();
+        Seconds.text = seconds.ToString();
+    }
+
+    private void CheckBeep(float prev, float cur, float sec, AudioSource beepSound)
+    {
+        if(prev>=sec && sec >= cur)
+        {
+            beepSound.Play();
+        }
+    }
+
+    private void TurnEndTimeSound(bool on)
+    {
+        if(on)
+        {
+            EndTimeSound.Play();
+        }
+        else
+        {
+            EndTimeSound.Stop();
+        }
     }
 }
